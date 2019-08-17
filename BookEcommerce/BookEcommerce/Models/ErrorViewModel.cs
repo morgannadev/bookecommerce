43a4608a8497d718a8666040d8f46adb0a7777b2
Morganna Giovanelli <morgcontato@gmail.com>
2019-08-17 20:31:20 -0300

    Commit inicial

---
+++ BookEcommerce/BookEcommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookEcommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
